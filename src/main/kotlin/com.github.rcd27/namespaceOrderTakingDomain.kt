c40881fdd40af24606d91f40184eee3a8f40c573
Stanislav Zemlyakov <redtom@yandex.ru>
2022-08-17 00:30:32 +0300

    master: add namespace for OrderTaking domain

---
+++ src/main/kotlin/com.github.rcd27/namespaceOrderTakingDomain.kt
@@ -0,0 +1,56 @@
+package com.github.rcd27
+
+import arrow.core.Either
+
+typealias WidgetCode = String // constraint starting with "W" then 4 digits
+typealias GizmoCode = String // constraint starting with "G" then 3 digits
+
+sealed class ProductCode {
+    data class Widget(val code: WidgetCode) : ProductCode()
+    data class Gizmo(val code: GizmoCode) : ProductCode()
+}
+
+typealias UnitQuantity = Int
+typealias KilogramQuantity = Float
+
+sealed class OrderQuantity {
+    data class Unit(val quantity: UnitQuantity) : OrderQuantity()
+    data class Kilos(val quantity: KilogramQuantity) : OrderQuantity()
+}
+
+typealias OrderId = Nothing
+typealias OrderLineId = Nothing
+typealias CustomerId = Nothing
+typealias CustomerInfo = Nothing
+typealias ShippingAddress = Nothing
+typealias BillingAddress = Nothing
+typealias Price = Nothing
+typealias BillingAmount = Nothing
+
+data class OrderLine(
+    val id: OrderLineId,
+    val orderId: OrderId,
+    val productCode: ProductCode,
+    val orderQuantity: OrderQuantity,
+    val price: Price,
+)
+
+data class Order(
+    val id: OrderId,
+    val customerId: CustomerId,
+    val shippingAddress: ShippingAddress,
+    val billingAddress: BillingAddress,
+    val orderLines: List<OrderLine>,
+    val amountToBill: BillingAmount,
+)
+
+data class UnvalidatedOrder(
+    val orderId: String,
+    val customerInfo: CustomerInfo,
+    val shippingAddress: ShippingAddress,
+)
+
+object PlaceOrderEvents // TODO: fill
+data class PlaceOrderError(val errors: List<Throwable>)
+
+typealias PlaceOrder = (UnvalidatedOrder) -> Either<PlaceOrderError, PlaceOrderEvents>
